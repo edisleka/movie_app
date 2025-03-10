b9f3f84810bf908221eee932119c91d2fc8f8a87
Edis Leka <edisleka@yahoo.gr>
2025-03-10 17:31:23 +0200

    project completed ++

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,22 @@
-import { View, Text } from 'react-native'
+import { icons } from '@/constants/icons'
+import { View, Text, Image, ScrollView } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5'
+
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView className='bg-primary flex-1 '>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerClassName='px-7 mb-100'
+      >
+        <View className='flex flex-row items-start justify-between mt-5'>
+          <Text className='text-white text-xl font-bold'>Profile</Text>
+          <FontAwesome5 name='bell' size={19} color='white' />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
+
 export default Profile
--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,8 +1,13 @@
-import { View, Text } from 'react-native'
+import { icons } from '@/constants/icons'
+import { View, Text, Image } from 'react-native'
+
 const Saved = () => {
   return (
-    <View>
-      <Text>Saved</Text>
+    <View className='bg-primary flex-1 px-10'>
+      <View className='flex flex-col flex-1 items-center justify-center gap-5'>
+        <Image source={icons.save} className='size-10' tintColor='#fff' />
+        <Text className='text-gray-500 text-base'>Save</Text>
+      </View>
     </View>
   )
 }
